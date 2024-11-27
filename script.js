2514615a000999146641997b15b934eaab23c841
Egemba-Edwin <157131854+Edwin-1010@users.noreply.github.com>
2024-11-27 17:26:18 +0100

    fixed the responsive design for the shop page

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
 const close = document.getElementById("close");
-const mainImg = getElementById("MainImg");
-const smallImg = getElementByclassName("small-img");
+const mainImg = document.getElementById("MainImg");
+const smallImg = document.getElementsByClassName("small-img");
 function showMenu() {
   if (bar) {
     bar.addEventListener("click", () => {
@@ -15,7 +15,20 @@ function showMenu() {
     });
   }
 }
-showMenu();
 function slider() {
-  smallImg[0].onclick = function () {};
+  smallImg[0].onclick = function () {
+    mainImg.src = smallImg[0].src;
+  };
+  smallImg[1].onclick = function () {
+    mainImg.src = smallImg[1].src;
+  };
+  smallImg[2].onclick = function () {
+    mainImg.src = smallImg[2].src;
+  };
+  smallImg[3].onclick = function () {
+    mainImg.src = smallImg[3].src;
+  };
 }
+
+showMenu();
+slider();
